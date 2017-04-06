94f3e044a3548e7731c30c19b860713a65d16746
Javier Negre <javi.negre@gmail.com>
2017-04-06 20:18:30 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express'),
+  app = express(),
+  path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/git-history', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/slides/git-history.html'));
+});
+
+app.get('/git-merge-vs-rebase', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/slides/git-merge-vs-rebase.html'));
+});
+
+app.get('*', (req, res) => {
+  res.send(`404 - ${req.url} route not found`);
+});
+
+module.exports = app;
